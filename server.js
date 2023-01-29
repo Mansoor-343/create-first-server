d8987630e3dcb2d976bc4d3de39e2ff4d900ace3
Mansoor-343 <mansoorahammed343@gmail.com>
2023-01-29 13:25:31 +0530

    installed 3rd party package(nodemon)

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
-const port = 8081;
+const port = 8081;  // local port number
 
-http.createServer((request,response) => {
+http.createServer((request,response) => { // callback function 
     response.writeHead(200,{'Content-Type' : 'text/html'});
     response.write("<h1> Hello , this from my server </h1>");
     response.end();
 })
-.listen(port,()=> {
+.listen(port,()=> { // call back function
     console.log(`Node.js server started on port ${port}`);
 });
-// http://localhost:8081
+// http://localhost:8081S
