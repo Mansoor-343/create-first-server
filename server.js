dc5e0b256993d60f45a5e10db272922015b7949a
Mansoor-343 <mansoorahammed343@gmail.com>
2023-01-29 15:02:50 +0530

    Updated server

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 const http = require('http');
 
-const port = 8081;  // local port number
+const port = 8081; 
 
-http.createServer((request,response) => { // callback function 
-    response.writeHead(200,{'Content-Type' : 'text/html'});
-    response.write("<h1> Hello , this from my server </h1>");
+const toDoList = ["complete Node Byte", "Play Cricket"]
+
+http
+.createServer((request,response) => {
+    const { method,url } = request;
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            response.writeHead(200,{"Content-Type": "text/html"});
+            response.write(toDoList.toString());
+        }  else if(method==="POST") {
+            let body = "";
+            request.on('error',(err) => {
+                console.error(err);
+
+            }).on('data',(chunk) => {
+                body +=chunk;
+                
+
+            }).on('end',() => {
+                body = JSON.parse(body);
+                console.log("data: ",body);
+            })
+
+        }
+        
+        else {
+            response.writeHead(501);
+        }
+
+    }else{
+        response.writeHead(404);
+    }
     response.end();
+
+   
 })
-.listen(port,()=> { // call back function
+.listen(port,()=> { 
     console.log(`Node.js server started on port ${port}`);
 });
-// http://localhost:8081S
